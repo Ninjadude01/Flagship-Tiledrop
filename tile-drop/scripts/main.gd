1ac1cb002ed2d43586d67dd3e0b984ac2457e139
Jason Liu <injadude01@gmail.com>
2026-02-21 09:43:30 -0800

    dslkfj

--- tile-drop/scripts/main.gd
+++ tile-drop/scripts/main.gd
@@ -20,56 +20,56 @@ func _ready() -> void:
 func rng() -> void:
 
 	number = randi_range(1, 10)
-	number2 = randi_range(1, 1000)
-	number = 1
+	number2 = randi_range(100, 900)
+	
 	if (number == 1):
 		var tile_1 = tile1.instantiate()
-		tile_1.global_position = global_position + Vector2 (number2, 0)
+		tile_1.global_position = global_position + Vector2 (number2, 50)
 		add_child(tile_1)
 		
 	if (number == 2):
 		var tile_2 = tile2.instantiate()
-		tile_2.global_position = global_position + Vector2 (number2, 0)
+		tile_2.global_position = global_position + Vector2 (number2, 50)
 		add_child(tile_2)
 		
 	if (number == 3):
 		var tile_3 = tile3.instantiate()
-		tile_3.global_position = global_position + Vector2 (number2, 0)
+		tile_3.global_position = global_position + Vector2 (number2, 50)
 		add_child(tile_3)
 		
 	if (number == 4):
 		var tile_4 = tile4.instantiate()
-		tile_4.global_position = global_position + Vector2 (number2, 0)
+		tile_4.global_position = global_position + Vector2 (number2, 50)
 		add_child(tile_4)
 		
 	if (number == 5):
 		var tile_5 = tile5.instantiate()
-		tile_5.global_position = global_position + Vector2 (number2, 0)
+		tile_5.global_position = global_position + Vector2 (number2, 50)
 		add_child(tile_5)
 		
 	if (number == 6):
 		var tile_6 = tile6.instantiate()
-		tile_6.global_position = global_position + Vector2 (number2, 0)
+		tile_6.global_position = global_position + Vector2 (number2, 50)
 		add_child(tile_6)
 		
 	if (number == 7):
 		var tile_7 = tile7.instantiate()
-		tile_7.global_position = global_position + Vector2 (number2, 0)
+		tile_7.global_position = global_position + Vector2 (number2, 50)
 		add_child(tile_7)
 		
 	if (number == 8):
 		var tile_8 = tile8.instantiate()
-		tile_8.global_position = global_position + Vector2 (number2, 0)
+		tile_8.global_position = global_position + Vector2 (number2, 50)
 		add_child(tile_8)
 		
 	if (number == 9):
 		var tile_9 = tile9.instantiate()
-		tile_9.global_position = global_position + Vector2 (number2, 0)
+		tile_9.global_position = global_position + Vector2 (number2, 50)
 		add_child(tile_9)
 		
 	if (number == 10):
 		var tile_10 = tile10.instantiate()
-		tile_10.global_position = global_position + Vector2 (number2, 0)
+		tile_10.global_position = global_position + Vector2 (number2, 50)
 		add_child(tile_10)
 		
 
--- tile-drop/scripts/tile_2.gd
+++ tile-drop/scripts/tile_2.gd
@@ -1,5 +1,19 @@
 extends RigidBody2D
+@export var bottom: PackedScene
 @onready var body: RigidBody2D = $"."
 
-func _ready() -> void:
+func _ready():
+
 	body.linear_velocity = Vector2(0, 400)
+
+	var bottom_scene = preload("res://scenes/bottom.tscn")
+	var bottoms = bottom_scene.instantiate()
+
+	bottoms.global_position = global_position + Vector2 (0, 0)
+
+	get_tree().current_scene.add_child(bottoms)
+	bottoms.area_hit.connect(_on_area_entered)
+
+	
+func _on_area_entered():
+	queue_free()
