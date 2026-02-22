a387e1cf140ece4b2fc90b5669cc6e09ce8f263a
SilentPrincess108 <rosan.syal@gmail.com>
2026-02-21 19:46:03 -0800

    working on generating random tiles

--- tile-drop/scripts/main.gd
+++ tile-drop/scripts/main.gd
@@ -1,30 +1,32 @@
 extends Node2D
 
 @onready var tile: Node2D = $Tile
-
-
-var tile1 = preload("res://scenes//tile1.tscn")
+@onready var score: Label = $CanvasLayer/ScorePanel/ScoreLabel
 
 var number = 0
-var pos = 0
+var pos = 100
 
 func _ready() -> void:
-	#pos = randi_range(100, 900)
-	pass
-	
-	#rng()
+	pos = randi_range(100, 900)
+	##rng()
 
-func _process(_delta: float) -> void:
+func _process(_delta: float):
 	tile.position = global_position + Vector2(pos, 50)
+	score.text = "Score: " + str(Globals.points)
+	
 
-func rng() -> void:
-	#number = randi_range(1, 10)
-	number = 1
-	if (number == 1):
-		var tile_1 = tile1.instantiate()
-		tile_1.global_position = global_position + Vector2 (pos, 50)
-		add_child(tile_1)
-
+#func rng() -> void:
+	##number = randi_range(1, 10)
+	#number = 1
+	#if (number == 1):
+		#var tile_1 = tile1.instantiate()
+		
+		#tile_1.global_position = global_position + Vector2 (pos, 50)
+		#add_child(tile_1)
 
 func _on_killzone_body_entered(_body: Node2D) -> void:
 	tile.hide()
+
+
+func _on_tile_next_tile() -> void:
+	pass # Replace with function body.
--- tile-drop/scripts/tile.gd
+++ tile-drop/scripts/tile.gd
@@ -1,6 +1,9 @@
 extends Node2D
 
 @onready var body: RigidBody2D = $RigidBody2D
+@onready var sprite: Sprite2D = $RigidBody2D/Sprite2D
+
+signal nextTile
 
 @export var keyDict = {
 	"1":false,
@@ -14,24 +17,51 @@ extends Node2D
 	"9":false
 }
 
+var keyDict2 = {
+	1: {"1":false, "2":true, "3":false, "4":false, "5":false, "6":true, "7":false, "8":false, "9":false},
+	2: {"1":false, "2":true, "3":false, "4":true, "5":true, "6":true, "7":false, "8":true, "9":false},
+	3: {"1":false, "2":true, "3":false, "4":true, "5":false, "6":true, "7":false, "8":false, "9":false},
+	4: {"1":false, "2":false, "3":false, "4":false, "5":false, "6":false, "7":false, "8":false, "9":false},
+	5: {"1":false, "2":false, "3":false, "4":false, "5":false, "6":false, "7":false, "8":false, "9":false},
+	6: {"1":false, "2":false, "3":false, "4":false, "5":false, "6":false, "7":false, "8":false, "9":false},
+	7: {"1":false, "2":false, "3":false, "4":false, "5":false, "6":false, "7":false, "8":false, "9":false},
+	8: {"1":false, "2":false, "3":false, "4":false, "5":false, "6":false, "7":false, "8":false, "9":false},
+	9: {"1":false, "2":false, "3":false, "4":false, "5":false, "6":false, "7":false, "8":false, "9":false},
+	10: {"1":false, "2":false, "3":false, "4":false, "5":false, "6":false, "7":false, "8":false, "9":false}
+}
+
 var keys = [1, 2, 3, 4, 5, 6 ,7 ,8 ,9]
 var keysPressed = 0
 
 var sequence = []
 var sequenceDetect = []
 var points = 0
+var textNum = 0
+var texture = Texture
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
+func _ready():
+	genRandTexture()
+	sprite.texture = texture
 	body.linear_velocity = Vector2(0, 400)
 	setSequence()
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(_delta: float) -> void:
-	if sequenceDetect.size() == sequence.size():
-		checkSequence()
-	restart()
 	
+func genRandTexture():
+	textNum = randi_range(1, 10)
+	texture = load("res://assets/pattern " + str(textNum) + ".png")
+
+func _process(_delta: float) -> void: #bug thingy
+	if checkSequence():
+		Globals.points += 5
+		print("Points Earned: " + str(Globals.points))
+		queue_free()
+		resetInput()
+		nextTile.emit()
+	elif !checkSequence():
+		restart()
+	else:
+		queue_free()
+		resetInput()
+		nextTile.emit()
 
 func _input(event: InputEvent):
 	if event is InputEventKey and event.is_released():
@@ -42,17 +72,18 @@ func restart():
 		print("restart")
 		resetInput()
 
-func checkSequence(): #fix this
-	for i in range(sequence.size()):
-		if sequenceDetect[i] == sequence[i]:
-			points += 1
-	if points == sequence.size():
-		print("Correct Sequence")
-		getSequence()
-		get_tree().quit()
-	else:
-		print("Incorrect Sequence! Try again!")
-		resetInput()
+func checkSequence():
+	if sequenceDetect.size() == sequence.size():
+		for i in range(sequence.size()):
+			if sequenceDetect[i] == sequence[i]:
+				points += 1
+		if points == sequence.size():
+			print("Correct Sequence")
+			getSequence()
+			return true
+		else:
+			print("Incorrect Sequence! Try again!")
+			return false
 
 func setSequence():
 	for key in keyDict:
